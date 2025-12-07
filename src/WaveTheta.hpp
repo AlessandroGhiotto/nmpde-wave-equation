9a0baf433ab2f731ecc8af28d0fa3a6c28ffe573
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-12-07 12:05:50 +0100

    added gridout header

--- src/WaveTheta.hpp
+++ src/WaveTheta.hpp
@@ -15,6 +15,7 @@
 
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_in.h>
+#include <deal.II/grid/grid_out.h>
 #include <deal.II/grid/grid_tools.h>
 #include <deal.II/grid/tria.h>
 
