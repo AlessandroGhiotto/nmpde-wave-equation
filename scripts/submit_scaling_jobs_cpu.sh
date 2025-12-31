2e5ee9ec803f8b3072eecd0f27a038341082417e
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-12-31 23:20:00 +0100

    updated scaling

--- scripts/submit_scaling_jobs_cpu.sh
+++ scripts/submit_scaling_jobs_cpu.sh
@@ -4,8 +4,8 @@ set -e
 echo "Submitting scaling jobs..."
 
 for p in 1 2 4 8 16; do
-    echo "  -> submitting scaling_${p}.pbs"
-    jobid="$(qsub "scaling_${p}.pbs")"
+    echo "  -> submitting scaling_cpu/scaling_${p}.pbs"
+    jobid="$(qsub "scaling_cpu/scaling_${p}.pbs")"
     echo "     jobid: ${jobid}"
 done
 
